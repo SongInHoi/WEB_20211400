6bd8b6cacd7f09e827f6004f3bee620966a44f58
songinhoi <koreasong97@naver.com>
2023-05-24 11:33:56 +0900

    23년5월24일 수정

---
+++ js/session.js
@@ -0,0 +1,83 @@
+/* // 12주차 ppt11장에서 웹 세션을 객체로 저장하는 기능을 위해 주석처리하고 아래 새롭게 만듬.
+function session_set() { //세션 저장
+    let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+    if (sessionStorage) {
+        let en_text = encrypt_text(password.value);
+        sessionStorage.setItem("Session_Storage_test", en_text);
+
+    } else {
+        alert("로컬 스토리지 지원 x");
+    }
+}
+*/
+function session_set(){ //세션 저장(객체)    
+    let id = document.querySelector("#floatingInput");
+    let password = document.querySelector("#floatingPassword");
+    let random = new Date(); // 랜덤 타임스탬프
+    
+    const obj = { // 객체 선언
+    id : id.value,
+    otp : random
+    }
+    
+    if (sessionStorage) {
+        const objString = JSON.stringify(obj); // 객체 -> JSON 문자열 변환
+        let en_text = encrypt_text(objString); // 암호화
+        sessionStorage.setItem("Session_Storage_object", objString);
+        sessionStorage.setItem("Session_Storage_encrypted", en_text);
+    } else {
+        alert("세션 스토리지 지원 x");
+    }   
+}
+
+
+function session_get() { //세션 읽기
+    if (sessionStorage) {
+       return sessionStorage.getItem("Session_Storage_encrypted"); //Session_Storage_test 였는데 기존 복호화된 값 확인을 위해서 키의 이름을 변경
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function session_check() { // 세션검사
+    if(sessionStorage.getItem("Session_Storage_encrypted")){
+        alert("이미 로그인 되었습니다.");
+        location.href='index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+
+function session_del() {//세션 삭제
+    // Check if the sessionStorage object exists
+    if (sessionStorage) {
+        // Retrieve data
+        sessionStorage.removeItem("Session_Storage_encrypted");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+function session_join_set(){ //세션 저장(객체) 12주차 ppt 18장
+    let f_name = document.querySelector("#firstName").value;
+    let l_name = document.querySelector("#lastName").value;
+    let b_day = document.querySelector("#birthdayDate").value;
+    let gender = document.querySelector("#inlineRadioOptions");
+    let email = document.querySelector("#emailAddress").value;
+    let p_number = document.querySelector("#phoneNumber").value;
+    let class_check = document.querySelector(".select form-control-lg");
+    let random = new Date(); // 랜덤 타임스탬프
+    
+    const newSignUp = new SignUp(f_name, l_name, b_day, gender, email, p_number, class_check, random);
+    console.log(newSignUp.fullName); // John Doe
+    console.log(newSignUp.contactInfo); // johndoe@email.com 123-456-7890
+    
+    if (sessionStorage) {
+        const objString = JSON.stringify(newSignUp); // 객체 -> JSON 문자열 변환
+        let en_text = encrypt_text(objString); // 암호화
+        sessionStorage.setItem("Session_Storage_object", objString);
+        sessionStorage.setItem("Session_Storage_encryted", en_text);
+    } else {
+        alert("세션 스토리지 지원 x");
+    }   
+}
